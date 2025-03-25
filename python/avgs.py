785de771617bc161932fbaae994e405385ae36ba
aloisorozco <57361077+aloisorozco@users.noreply.github.com>
2025-03-25 16:51:20 -0400

    removed players from db
    currently having an issue with exceeding max reads while testing

--- python/avgs.py
+++ python/avgs.py
@@ -7,10 +7,12 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-from avgPlayer import Player
+from classes.avg_player import Player
 
 cred = credentials.Certificate("credentials.json")
 firebase_admin.initialize_app(cred)
+
+non_playoff_teams = {'POR', 'CHO', 'UTA', 'WAS', 'SAS', 'PHI', 'BRK', 'NOP'}
 
 url = "https://www.basketball-reference.com/leagues/NBA_2025_per_game.html"
 result = requests.get(url)
@@ -35,8 +37,8 @@ for tr in trs:
     
     try:
         p = Player(tr.find("td", {"data-stat" : "name_display"}).string, tr.find("td", {"data-stat" : "pos"}).string, tr.find("td", {"data-stat" : "team_name_abbr"}).string, tr.find("td", {"data-stat" : "pts_per_g"}).string, tr.find("td", {"data-stat" : "trb_per_g"}).string, tr.find("td", {"data-stat" : "ast_per_g"}).string, tr.find("td", {"data-stat" : "blk_per_g"}).string, tr.find("td", {"data-stat" : "stl_per_g"}).string, tr.find("td", {"data-stat" : "tov_per_g"}).string)
-        # removing players under 10 fantasy points (no one will draft them)
-        if p.avgFantasyPoints < 10:
+        # removing players under 18 fantasy points (no one will draft them) and non playoff teams
+        if p.avgFantasyPoints < 18 or p.team in non_playoff_teams:
             continue
         
         if 'TM' in p.team:
